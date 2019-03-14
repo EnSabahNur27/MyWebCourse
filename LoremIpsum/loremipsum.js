5d814247f3405ce7aea234cd6ddf1b79012f7278
Jay Paulonis <jaypaulonis@alumni.pitt.edu>
2019-03-14 10:01:38 -0400

    Reactive html

---
+++ LoremIpsum/loremipsum.js
@@ -0,0 +1,5 @@
+let ipsum = {hodor:'hodor hodor hodor hodor hodor', gandalf:'Thou shall not pass', heman:'By the power of greyskull'};
+
+function addIpsum() {
+    document.getElementById("hodor").innerHTML = ipsum['hodor'];
+}
